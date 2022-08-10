3d8275202a563b16926b88d5a0b82c5cb14d5514
Brayan Jules <brayan1213@gmail.com>
2022-08-10 11:21:52 -0400

    wip: added database credentials to get from the env variables

--- src/sources/twitter/src/main/scala/Main.scala
+++ src/sources/twitter/src/main/scala/Main.scala
@@ -9,8 +9,9 @@ object Main extends App{
 
   val source:SourceOperator = new SourceTwitterAPIOperator()
   val sink:SinkOperator = new SinkPostgresOperator()
-
-  val generatedData  = source.consumeList("1262002053683044352",100)
+  //DEV ENV LIST: 1557027916193611777
+  //PROD ENV LIST: 1262002053683044352
+  val generatedData  = source.consumeList("1557027916193611777",100)
   sink.produce("source.tweets",generatedData,Seq.empty[(String,String)])
 
 
